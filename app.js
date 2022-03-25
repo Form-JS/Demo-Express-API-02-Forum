61daa2bec63147c4d70320c18a133ebcbf496864
Aurélien <aurelien.strimelle@bstorm.be>
2022-03-25 10:17:56 +0100

    Initialize Web API

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('express-async-errors');
+
+// Load env file
+require('dotenv-flow').config;
+
+// Get env variable
+const { PORT, NODE_ENV } = process.env;
+
+// Create Web API
+const app = express();
+
+// Add Middlewares
+app.use(express.json());
+
+// TODO Initialize Database
+// ...
+
+// TODO Add Routing
+// ...
+
+// Start Web API
+app.listen(PORT, () => {
+    console.log(`Web API up on port ${PORT}  [${NODE_ENV}]`);
+});
