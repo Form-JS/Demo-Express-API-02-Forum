8a03926657c0c4a691bc174f6afe9500a5c20a87
Aurélien <aurelien.strimelle@bstorm.be>
2022-03-30 20:04:52 +0200

    Refactoring JWT Utils - Support Promise

--- middlewares/authentificate-jwt.js
+++ middlewares/authentificate-jwt.js
@@ -2,7 +2,13 @@ const { decodeJWT } = require('../utils/jwt-utils');
 const db = require('../models');
 const { Op } = require('sequelize');
 
+/**
+ * Middleware d'authentification via les JSON Web Token
+ * @param {{adminRight: boolean}} options 
+ * @returns {(req: Request, res: Response, next: NextFunction) => Void}
+ */
 const authentificateJwt = ({ adminRight = false }) => {
+
     /**
       * Middleware pour gérer les jwt
       * @param {Request} req 
@@ -10,49 +16,53 @@ const authentificateJwt = ({ adminRight = false }) => {
       * @param {NextFunction} next 
       */
     return async (req, res, next) => {
-        // - Récuperation du header d'authenfication
+        // Récuperation du header d'authenfication
+        // -> Exemple de résultat: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6..."
         const authHeader = req.headers['authorization'];
-        // Résultat: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6..."
 
         // Récuperation du JWT
         const token = authHeader && authHeader.split(' ')[1];
 
-        // Si aucun token recu
+        // Si aucun token n'a été recu, erreur 401.
         if (!token) {
             return res.sendStatus(401);
         }
 
-        // Si un token
+        // Récuperation des données du JWT
+        let tokenData;
         try {
-            // Récuperation des données
-            const tokenData = decodeJWT(token);
-
-            // Si 'AdminRight' -> Verification si l'utilisateur est admin
-            if (adminRight) {
-                //  Check en DB
-                const admin = await db.Member.findOne({
-                    where: {
-                        [Op.and]: [
-                            { id: tokenData.id },
-                            { isAdmin: true }
-                        ]
-                    }
-                });
-
-                if (!admin) {
-                    return res.sendStatus(403);
-                }
-            }
-
-            // Ajout des infos du token a l'object "request" d'express
-            req.user = tokenData;
-
-            // On continue :)
-            next();
+            // Extraction des données
+            tokenData = await decodeJWT(token);
         }
         catch (error) {
+            // En cas d'erreur, envoi d'un erreur
             return res.sendStatus(403);
         }
+
+        // Vérification des droits de l'utilisateur si le flag "AdminRight" est présent
+        if (adminRight) {
+            // Validation des droits via la base de donnée 
+            // -> Certitude d'avoir les données à jours
+            const admin = await db.Member.findOne({
+                where: {
+                    [Op.and]: [
+                        { id: tokenData.id },
+                        { isAdmin: true }
+                    ]
+                }
+            });
+
+            // Erreur 403 si l'utilisateur n'a pas les droits
+            if (!admin) {
+                return res.sendStatus(403);
+            }
+        }
+
+        // Ajout des infos du token a l'object "request" de Express
+        req.user = tokenData;
+
+        // On continue :)
+        next();
     };
 };
 
--- utils/jwt-utils.js
+++ utils/jwt-utils.js
@@ -1,42 +1,49 @@
 const jwt = require('jsonwebtoken');
 
 const generateJWT = ({ id, pseudo, isAdmin }) => {
-    const data = { id, pseudo, isAdmin };
-    const secret = process.env.JWT_SECRET;
-    const audience = process.env.JWT_AUDIENCE;
-    const issuer = process.env.JWT_ISSUER;
-    const expiresIn = '12h'; // Format: https://github.com/vercel/ms
+    return new Promise((resolve, reject) => {
+        const data = { id, pseudo, isAdmin };
+        const options = {
+            algorithm: 'HS512',  // HS256 par default
+            secret: process.env.JWT_SECRET,
+            audience: process.env.JWT_AUDIENCE,
+            issuer: process.env.JWT_ISSUER,
+            expiresIn: '12h'    // Format: https://github.com/vercel/ms
+        };
 
-    const token = jwt.sign(data, secret, {
-        audience,
-        issuer,
-        expiresIn,
-        algorithm: 'HS512'  // HS256 par default
+        jwt.sign(data, secret, options, (error, token) => {
+            if (error) {
+                return reject(error);
+            }
+            
+            const expire = new Date(jwt.decode(token).exp * 1000).toISOString();
+            resolve({ token, expire });
+        });
     });
-    const expire = new Date(jwt.decode(token).exp * 1000).toISOString();
-
-    return {
-        token,
-        expire
-    };
 };
 
 const decodeJWT = (token) => {
     if (!token) {
-        throw new Error('Indalid JWT');
+        return Promise.reject(new Error('Indalid JWT'));
     }
 
-    const optionsVerify = {
-        audience: process.env.JWT_AUDIENCE,
-        issuer: process.env.JWT_ISSUER,
-    };
-    const data = jwt.verify(token, process.env.JWT_SECRET, optionsVerify);
-
-    return {
-        id: data.id,
-        pseudo: data.pseudo,
-        isAdmin: data.isAdmin
-    };
+    return new Promise((resolve, reject) => {
+        const optionsVerify = {
+            audience: process.env.JWT_AUDIENCE,
+            issuer: process.env.JWT_ISSUER,
+        };
+        jwt.verify(token, process.env.JWT_SECRET, optionsVerify, (error, data) => {
+            if(error) {
+                return reject(error);
+            }
+            
+            resolve({
+                id: data.id,
+                pseudo: data.pseudo,
+                isAdmin: data.isAdmin
+            });
+        });
+    });
 };
 
 module.exports = {
