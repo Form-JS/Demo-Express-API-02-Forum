c740ebedc31817b1576750b6453e629a9dee0030
Aurélien <aurelien.strimelle@bstorm.be>
2022-03-30 12:12:19 +0200

    Add structure for feature login/register

---
+++ controllers/auth-controller.js
@@ -0,0 +1,14 @@
+const authController = {
+
+    register: async (req, res) => {
+
+        res.sendStatus(501);
+    },
+
+    login: async (req, res) => {
+
+        res.sendStatus(501);
+    }
+};
+
+module.exports = authController;
